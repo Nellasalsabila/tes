a97c066543c4345d210bbd21a50c9c089b687006
Nellasalsabila <nella_salsabila_33rpl@student.smktelkom-mlg.sch.id>
2024-07-31 14:58:19 +0700

    Add files via upload

---
+++ tes.java
@@ -0,0 +1,11 @@
+public class tes{
+    public static void main(String[] args) {
+
+        System.out.println();
+        System.out.println("Nama:Nella salsabila");
+        System.out.println("Alamat:Mendalan wanggi kec.wagir");
+        System.out.println("smp:Negri 01 wagir");
+        System.out.println("No Hp:089512294876");
+
+    }
+}
